3e62c98a8c64d70aa7b5a6b0e7aafbdc49e02f99
Liliana Fu <lilifuye72@gmail.com>
2023-04-25 12:19:51 -0500

    fix: remove primeflex dependecy

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import i18n from "./i18n.js";
 // Prime Vue
 import PrimeVue from "primevue/config";
 import 'primevue/resources/primevue.min.css';
-import 'primeflex/primeflex.css';
 
 // App Theme
 import 'primevue/resources/themes/soho-light/theme.css';
