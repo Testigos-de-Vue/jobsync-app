6b139bee60d3bc0cb593fccf535e072ee704ced9
aleiva <aleiva1700@gmail.com>
2023-04-26 15:51:29 -0500

    fix: update missing alt attribute on image & remove unnecessary comments

--- src/shared/components/avatar-card.component.vue
+++ src/shared/components/avatar-card.component.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="flex items-center gap-4">
-<!--    <div class="h-20 w-20 overflow-hidden rounded-full">-->
-<!--      <div-->
-<!--        :style="{backgroundImage:`url(${ user.profileImageUrl })`}"-->
-<!--        class="h-full w-auto bg-center bg-contain bg-no-repeat">-->
-<!--      </div>-->
-<!--    </div>-->
-      <img class="h-20 w-20 object-cover rounded-full" :src="user.profileImageUrl" alt="">
+    <img
+      class="h-20 w-20 object-cover rounded-full"
+      :src="user.profileImageUrl"
+      :alt="`${user.name} profile picture`"
+    >
     <div class="text-white">
       <p class="text-xl font-bold">{{ user.name }}</p>
       <p>{{ user.subTitle }}</p>
