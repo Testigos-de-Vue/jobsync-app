027587c2040902bc85e12cca3d35727f5a22db50
aleiva <aleiva1700@gmail.com>
2023-04-30 19:46:26 -0500

    feat: add app settings and profile settings

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import {createRouter, createWebHistory} from "vue-router";
 import Login from "../authentication/pages/login.component.vue";
 import Register from "../authentication/pages/register.component.vue";
 import CreateOrganization from "../organizations/pages/create-organization.component.vue";
+import Settings from "../settings/pages/settings.component.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/login', name: 'Login', component: Login },
     { path: '/register', name: 'Register', component: Register },
-    { path: '/create-organization', name: 'Create Organization', component: CreateOrganization }
+    { path: '/create-organization', name: 'Create Organization', component: CreateOrganization },
+    { path: '/settings', name: 'Settings', component: Settings }
   ]
 });
 
---
+++ src/settings/components/language-switcher.component.vue
@@ -0,0 +1,28 @@
+<template>
+  <pv-dropdown
+    class=""
+    v-model="locale"
+    :options="languages"
+  />
+</template>
+
+<script>
+export default {
+  name: "language-switcher",
+  data() {
+    return {
+      languages: ['English', 'Español'],
+      locale: 'English'
+    }
+  },
+  watch: {
+    locale(selectedLanguage) {
+      this.$i18n.locale = selectedLanguage.slice(0, 2).toLowerCase();
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
