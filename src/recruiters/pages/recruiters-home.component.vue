46ce44a38e4af5cc79f9d6fba76be101ba11d7c9
RamiroVT <u202123714@upc.edu.pe>
2023-05-02 11:35:30 -0500

    feature: add recruiters homepage and new asset

---
+++ src/recruiters/pages/recruiters-home.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center mb-6">
+    <greeting/>
+  </div>
+  <div class="w-full grid grid-cols-1 sm:grid-cols-2 gap-4">
+    <div class="flex justify-center mb-7">
+      <calendar/>
+    </div>
+    <div class="">
+      <pending-tasks/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Calendar from "../../recruiters/components/calendar.component.vue";
+import Greeting from "../../shared/components/greeting.component.vue";
+import PendingTasks from "../components/pending-tasks.component.vue";
+export default {
+  name: "recruiters-home",
+  components: {Calendar, Greeting, PendingTasks},
+}
+</script>
+
+<style scoped>
+</style>
