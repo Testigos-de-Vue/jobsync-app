88b813ccf514fc9c42df60c70dccbf1e8642393b
Liliana Fu <lilifuye72@gmail.com>
2023-05-02 13:40:35 -0500

    fix: fix login form i18n

--- src/authentication/components/login-form.component.vue
+++ src/authentication/components/login-form.component.vue
@@ -5,24 +5,24 @@
           <img class="h-8 w-auto" src="/jobsync-logo.png" alt="JobSync Logo">
         </div>
         <div>
-          <h1 class="text-center font-bold text-3xl">Log In</h1>
+          <h1 class="text-center font-bold text-3xl">{{ $t('auth.login') }}</h1>
         </div>
         <div class="w-full">
           <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
-            E-mail
+            {{ $t('auth.e-mail') }}
           </label>
           <pv-input id="email" class="w-full" placeholder="example@mail.com" type="email" />
         </div>
       <div class="w-full">
         <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
-          Password
+          {{ $t('auth.password') }}
         </label>
         <pv-input id="password" class="w-full" type="password" placeholder="••••••••••••" />
       </div>
       <pv-button class="w-full text-center" type="submit" label="Log In" />
       <div class="grid place-items-center">
-        <p class="text-primary"><a class="hover:font-medium" href="#">Forgot Password?</a></p>
-        <p>Don't have an account? <a class="hover:font-medium text-primary" href="#">Sign up</a></p>
+        <p class="text-primary"><a class="hover:font-medium" href="#">{{ $t('auth.forgot-password') }}</a></p>
+        <p>{{ $t('auth.account-question') }} <a class="hover:font-medium text-primary" href="#">{{ $t('auth.sign-up') }}</a></p>
       </div>
     </div>
   </form>
