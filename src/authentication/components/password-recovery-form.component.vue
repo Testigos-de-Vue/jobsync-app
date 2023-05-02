236066545678288d5bd4b70df454cf69aaa0505b
Liliana Fu <lilifuye72@gmail.com>
2023-05-02 14:03:00 -0500

    fix: fix i18n and sidebar visibility

--- src/authentication/components/password-recovery-form.component.vue
+++ src/authentication/components/password-recovery-form.component.vue
@@ -5,28 +5,28 @@
   <form class="bg-white shadow-xl rounded px-8 pt-6 pb-8">
     <div class="flex flex-col space-y-6">
       <div>
-        <h1 class="text-left font-bold text-3xl">Password Recovery</h1>
+        <h1 class="text-left font-bold text-3xl">{{ $t('auth.password-recovery.title') }}</h1>
       </div>
       <div>
-        <p class="text-gray-500">Enter new password</p>
+        <p class="text-gray-500">{{ $t('auth.password-recovery.enter-new-password') }}</p>
       </div>
       <div class="w-full">
         <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
-          Password
+          {{ $t('auth.password') }}
         </label>
         <pv-input id="password" class="w-full" placeholder="••••••••••••" type="password  " />
       </div>
       <div class="w-full">
         <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
-          Confirm Password
+          {{ $t('auth.confirm-password') }}
         </label>
         <pv-input id="password" class="w-full" type="password" placeholder="••••••••••••" />
       </div>
-      <pv-button class="w-full text-center" type="submit" label="Change password" />
+      <pv-button class="w-full text-center" type="submit" label="Change Password" />
       <div class="grid place-items-center">
-          <p>Go back to
+          <p>{{ $t('auth.password-recovery.go-back') }} 
             <router-link to="/login">
-              <span class="hover:font-medium text-primary">Log In</span>
+              <span class="hover:font-medium text-primary">{{ $t('auth.login') }}</span>
             </router-link>
           </p>
       </div>
