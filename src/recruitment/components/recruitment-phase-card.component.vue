27c197a811950404d6def86ec0376fc8beb94bb6
sayufu <lilifuye72@gmail.com>
2023-06-01 19:05:45 -0500

    feat: add recruitment phases card

---
+++ src/recruitment/components/recruitment-phase-card.component.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-full flex items-center justify-center">
+    <div :class="[`bg-gray-100`, `dark:bg-gray-800`, 'flex border rounded-xl px-4 py-4 w-72 h-[630px]']">
+      <div id="header" class="flex justify-between items-center w-full h-fit px-1 py-1">
+        <h1 class="font-semibold text-xl">{{ phase.title }}</h1>
+        <div class="flex">
+          <pv-button icon="pi pi-ellipsis-h" text rounded size="large" severity="secondary" outlined
+                     aria-label="Phase dialog expander" />
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "recruitment-phase-card",
+  props: {
+    phase: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/recruitment/pages/recruitment-dashboard.component.vue
+++ src/recruitment/pages/recruitment-dashboard.component.vue
@@ -1,57 +1,54 @@
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-[auto_1fr] gap-6 items-center justify-center">
-    <div class="flex flex-col gap-4">
-      <pv-button
-          rounded
-          outlined
-          @click="$router.back()"
-          :label="$t('general.go-back-btn-label')"
-          icon="pi pi-arrow-left"
-      />
-      <pv-button
-          rounded
-          outlined
-          aria-label="Add phase"
-          :label="$t('recruitment.dashboard.add-phase-lbl')"
-          @click=""
-      />
-      <pv-button
-          rounded
-          outlined
-          aria-label="Add Candidates"
-          :label="$t('recruitment.dashboard.add-candidates-lbl')"
-          @click=""
-      />
-      <pv-button
-          rounded
-          outlined
-          aria-label="Recruitment Settings"
-          :label="$t('recruitment.dashboard.recruitment-settings-lbl')"
-          @click=""
-      />
-    </div>
-    <div class="gap-4">
-      <div class="space-y-2">
-        <div class="flex gap-2 overflow-x-auto">
-          <div class="bg-purple-600 dark:bg-purple-950 px-2 py-2 rounded-xl text-white">
-            <p class="font-medium">
-              <i class="pi pi-user" />
-              {{ recruitment.candidates.length }}
-              {{ $t('recruitment.dashboard.candidates-counter')}}
-            </p>
+  <div class="space-y-6">
+    <div class="grid grid-cols-1 lg:grid-cols-[auto_1fr] gap-6 items-center justify-center">
+      <div class="flex flex-col gap-4">
+        <pv-button rounded outlined
+            @click="$router.back()"
+            :label="$t('general.go-back-btn-label')"
+            icon="pi pi-arrow-left"
+        />
+        <pv-button rounded outlined
+            aria-label="Add phase"
+            :label="$t('recruitment.dashboard.add-phase-lbl')"
+            @click=""
+        />
+        <pv-button rounded outlined
+            aria-label="Add Candidates"
+            :label="$t('recruitment.dashboard.add-candidates-lbl')"
+            @click=""
+        />
+        <pv-button rounded outlined
+            aria-label="Recruitment Settings"
+            :label="$t('recruitment.dashboard.recruitment-settings-lbl')"
+            @click=""
+        />
+      </div>
+      <div class="gap-4">
+        <div class="space-y-2">
+          <div class="flex gap-2 overflow-x-auto">
+            <div class="bg-purple-600 dark:bg-purple-950 px-2 py-2 rounded-xl text-white">
+              <p class="font-medium">
+                <i class="pi pi-user" />
+                {{ recruitment.phases.length }}
+                {{ $t('recruitment.dashboard.candidates-counter')}}
+              </p>
+            </div>
+            <div class="bg-red-600 dark:bg-red-950 px-2 py-2 rounded-xl text-white">
+              <p class="font-medium">
+                <i class="pi pi-user" />
+                {{ recruitment.phases.length }}
+                {{ $t('recruitment.dashboard.phases-counter')}}
+              </p>
+            </div>
           </div>
-          <div class="bg-red-600 dark:bg-red-950 px-2 py-2 rounded-xl text-white">
-            <p class="font-medium">
-              <i class="pi pi-user" />
-              {{ recruitment.phases.length }}
-              {{ $t('recruitment.dashboard.phases-counter')}}
-            </p>
+          <div class="flex">
+            <recruitment-card :recruitment="recruitment" />
           </div>
-        </div>
-        <div>
-          <recruitment-card :recruitment="recruitment"/>
         </div>
       </div>
+    </div>
+    <div class="flex space-x-4 overflow-x-auto">
+      <recruitment-phase-card v-for="phase in recruitment.phases" :key="phase.id" :phase="phase"/>
     </div>
   </div>
 </template>
@@ -59,14 +56,15 @@
 <script>
 import RecruitmentCard from "../../shared/components/recruitment-card.component.vue";
 import {RecruitmentApiService} from "../../shared/services/recruitment.service.js";
+import RecruitmentPhaseCard from "../components/recruitment-phase-card.component.vue";
 
 export default {
   name: "recruitment-dashboard",
-  components: { RecruitmentCard },
+  components: { RecruitmentPhaseCard, RecruitmentCard },
   data() {
     return {
       recruitmentApi: new RecruitmentApiService(),
-      recruitment: {},
+      recruitment: {}
     }
   },
   created() {
