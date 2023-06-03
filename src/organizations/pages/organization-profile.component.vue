105dbe13c504c82405251a3183e1fb2794eb4407
sayufu <lilifuye72@gmail.com>
2023-06-02 19:30:43 -0500

    fix: fix members table in organization profile as the data was not displayed properly

--- src/organizations/pages/organization-profile.component.vue
+++ src/organizations/pages/organization-profile.component.vue
@@ -2,73 +2,60 @@
   <div class="space-y-4">
     <div>
       <pv-button
-        rounded
-        outlined
-        aria-label="Go back"
-        @click="$router.back()"
+          rounded
+          outlined
+          aria-label="Go back"
+          @click="$router.back()"
       >
         <i class="pi pi-arrow-left mr-2" />{{ $t('general.go-back-btn-label')}}
       </pv-button>
       <div class="flex space-x-4 justify-center items-center py-4">
         <div>
-          <img 
-          :src="currentOrganization.avatarUrl" 
-          alt="Organization Profile Picture"
-          class="rounded-3xl w-32 h-32 object-cover hover:scale-95 duration-200"/>
+          <img
+              :src="currentOrganization.avatarUrl"
+              alt="Organization Profile Picture"
+              class="rounded-3xl w-32 h-32 object-cover hover:scale-95 duration-200"/>
         </div>
         <div class="space-y-2">
           <div>
             <h1 class="text-primary font-medium text-xl">Working on</h1>
             <p class="font-bold text-3xl">{{ currentOrganization.name }}</p>
           </div>
-          <pv-button
-            rounded
-            outlined
-            :label="$t('organization-profile.organization-settings-label')"/>
+          <pv-button rounded outlined
+              :label="$t('organization-profile.organization-settings-label')"/>
         </div>
       </div>
     </div>
     <div class="justify-center items-center">
-      <members-table 
-      :members = "currentOrganization.members"
-      :users = "users"
-      />
+      <members-table :members = "currentOrganization.members" />
     </div>
   </div>
-  </template>
-  
-  <script>
-  import { OrganizationApiService } from "../../shared/services/organizations.service.js"
-  import { UsersApiService } from  "../../shared/services/users.service.js"
-  import MembersTable from "../components/organization-members-table.component.vue"
+</template>
 
-  export default {
-    name: "organization-profile",
-    components: { MembersTable },
-    data(){
-      return {
-        currentOrganizationId: 1,
-        currentOrganization: {},
-        organizationService: new OrganizationApiService,
-        users: [],
-        usersService: new UsersApiService,
-      }
-    },
-    created(){
-      this.organizationService.getCurrentOrganization(this.currentOrganizationId)
+<script>
+import MembersTable from "../components/organization-members-table.component.vue"
+import {OrganizationApiService} from "../../shared/services/organizations.service.js";
+
+export default {
+  name: "organization-profile",
+  components: { MembersTable },
+  data(){
+    return {
+      currentOrganizationId: 1,
+      currentOrganization: {},
+      organizationService: new OrganizationApiService(),
+    }
+  },
+  created(){
+    this.organizationService.getCurrentOrganization(this.currentOrganizationId)
         .then(response => this.currentOrganization = response.data)
         .catch(error => {
           console.error('An error occurred with organization:', error);
         });
-      this.usersService.getAll()
-        .then(response => this.users = response.data)
-        .catch(error => {
-          console.error('An error occurred with users:', error);
-        });
-    }
   }
-  </script>
-  
-  <style scoped>
-  
-  </style>
+}
+</script>
+
+<style scoped>
+
+</style>
