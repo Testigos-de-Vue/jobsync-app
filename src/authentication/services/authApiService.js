bdae7fb0223dc69e7af12685d695fd343429a322
Andrés Leiva <aleiva1700@gmail.com>
2023-06-23 15:45:16 -0500

    refactor: improve auth api service

--- src/authentication/services/authApiService.js
+++ src/authentication/services/authApiService.js
@@ -1,11 +1,42 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/api/v1'
+  baseURL: 'http://localhost:56480/api/v1/users'
 });
 
 export class AuthApiService {
+
+  signUp(firstName, lastName, email, password, phoneNumber, isSubscribedToNewsletter, isRecruiter) {
+    return http.post('sign-up', {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      phoneNumber: phoneNumber,
+      isSubscribedToNewsletter: isSubscribedToNewsletter,
+      isRecruiter: isRecruiter
+    })
+  }
+
+  signIn(email, password) {
+    return http.post('sign-in', {
+      email: email,
+      password: password
+    })
+      .then(res => {
+        if (res.data.token) {
+          localStorage.setItem('user', JSON.stringify(res.data));
+        }
+        return res.data;
+      })
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
   getUserById(id) {
-    return http.get(`users/${id}`);
+    // TODO: Do implementation
+    return http.get(`${id}`);
   }
 }
